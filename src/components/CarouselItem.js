904d404acf90b97f1a48925a8919a9861a764eb9
Thor W <6273158@rts-365.dk>
2021-03-12 09:02:56 +0100

    added alt

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -3,7 +3,7 @@ import "./CarouselItem.scss";
 export default function CarouselItem(props) {
     return (
         <div className="carouselItem">
-            <img src={props.image} />
+            <img src={props.image} alt="" />
             <div>
                 <p>{props.copy}</p>
                 <a href={props.ctaLink}>{props.ctaCopy}</a>
--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -3,7 +3,7 @@ import "./TestimonialCard.scss";
 export default function TestimonialCard(props) {
     return (
         <article className="testimonialCard">
-            <img src={props.image} />
+            <img src={props.image} alt="" />
             <p>{props.copy}</p>
         </article>
     )
